859c6a2e6cb8a17c667ed5edc8bca7e9d73cbd79
aleman87 <jaleman@ucdavis.edu>
2019-10-24 21:59:09 -0700

    updated vin
    now prints everything it finds

--- vin.py
+++ vin.py
@@ -1,8 +1,6 @@
 import json
 import requests
 
-#import pytz
-#import datetime as datetime
 from dateutil import parser
 
 import sys
@@ -12,14 +10,16 @@ try:
     vin = str(sys.argv[1]) 
     year = str(sys.argv[2])
     url = 'https://vpic.nhtsa.dot.gov/api/vehicles//DecodeVin/' + vin + '*BA?format=JSON&modelyear=' + year
+    print(f"Url: {url}")
     response = requests.get(url)
+    print(f"response:{response}")
     data = response.json()
+    # print(f"data: {data}")
     make = data['Results'][5]['Value']
     model = data['Results'][7]['Value']
     veh_type = data['Results'][12]['Value']
+    for result in range(len(data['Results'])):
+        print(data['Results'][result])
 
-    print('Make: '  + make)
-    print('Model: ' + model)
-    print('Type: '  + veh_type )
 
-except Exception as e: print(e)           
+except Exception as e: print(e)        
